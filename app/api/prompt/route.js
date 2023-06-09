006a9fe74e82018387eecc386f72241df841eb80
Tanay <tanay_baswa@yahoo.com>
2023-06-09 11:35:55 -0700

    v7.2 changed some things

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -13,6 +13,6 @@ export const GET = async (request) => {
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response(JSON.stringify("Failed to fetch all prompts"), { status: 500 })
     }
 } 

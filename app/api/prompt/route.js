b390c17688b90872f1c16b2a3579a5ca451ab1ca
Tanay <tanay_baswa@yahoo.com>
2023-06-09 11:39:21 -0700

    v7.3

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,10 +9,10 @@ export const GET = async (request) => {
 
         // console.log(updated.modifiedCount);
 
-        const prompts = await Prompt.find({}).populate('creator')
+        const prompts = await Prompt.find({}).populate('creator');
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200 });
     } catch (error) {
-        return new Response(JSON.stringify("Failed to fetch all prompts"), { status: 500 })
+        return new Response("Failed to fetch all prompts", { status: 500 });
     }
 } 
